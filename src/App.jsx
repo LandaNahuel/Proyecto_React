6f3dd54f806daec75d03ce4b39a25f6de61297ff
Nahuel Landa <landanahuel@icloud.com>
2023-03-02 18:30:01 -0300

    primer commit

---
+++ src/App.jsx
@@ -0,0 +1,10 @@
+import NavBar from "./componentes/Navbar";
+function App()  {
+  return (
+    <div>
+      <NavBar />
+    </div>
+  )
+}
+
+export default App;
---
+++ src/componentes/Navbar/index.jsx
@@ -0,0 +1,8 @@
+import "./navbar.css";
+
+function NavBar(){
+    return <div></div>;
+
+}
+
+export default NavBar
